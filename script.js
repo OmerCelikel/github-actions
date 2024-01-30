4f11838065b1ff3403046bc6fea88b1000308ec8
Ömer Oğuz <44753206+OmerCelikel@users.noreply.github.com>
2024-01-30 10:48:02 +0300

    js create

---
+++ script.js
@@ -0,0 +1,9 @@
+// Örnek bir JavaScript kodu
+document.addEventListener("DOMContentLoaded", function() {
+    // Sayfa yüklendiğinde bu kod çalışır
+    console.log("Sayfa yüklendi.");
+    
+    // Örnek bir işlem
+    var header = document.querySelector("header");
+    header.style.backgroundColor = "red";
+});
